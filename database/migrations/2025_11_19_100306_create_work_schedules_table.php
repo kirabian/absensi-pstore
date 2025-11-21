c576d9acf4146c6f4915ed0f5730f25a9dce897b
kirabian <fabiansyahalghiffarireal@gmail.com>
2025-11-21 10:14:10 +0700

    89

--- database/migrations/2025_11_19_100306_create_work_schedules_table.php
+++ database/migrations/2025_11_19_100306_create_work_schedules_table.php
@@ -14,22 +14,22 @@ return new class extends Migration
         Schema::create('work_schedules', function (Blueprint $table) {
             $table->id();
             $table->foreignId('branch_id')
-                  ->nullable()
-                  ->constrained('branches')
-                  ->onDelete('cascade');
+                ->nullable()
+                ->constrained('branches')
+                ->onDelete('cascade');
             $table->foreignId('division_id')
-                  ->nullable()
-                  ->constrained('divisions')
-                  ->onDelete('cascade');
+                ->nullable()
+                ->constrained('divisions')
+                ->onDelete('cascade');
             $table->string('schedule_name');
-            $table->time('check_in_start');
-            $table->time('check_in_end');
-            $table->time('check_out_start');
-            $table->time('check_out_end');
+            $table->time('check_in_start')->nullable();
+            $table->time('check_in_end')->nullable();
+            $table->time('check_out_start')->nullable();
+            $table->time('check_out_end')->nullable();
             $table->boolean('is_default')->default(false);
             $table->boolean('is_active')->default(true);
             $table->timestamps();
-            
+
             // Index untuk performa
             $table->index(['branch_id', 'division_id']);
             $table->index('is_default');
